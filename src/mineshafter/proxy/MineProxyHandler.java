c074764afff1d59583166ccaf799d331f33655cd
Ryan Sullivan <kayoticsully@gmail.com>
2012-04-11 18:54:52 -0400

    Server Snoop Test

--- src/mineshafter/proxy/MineProxyHandler.java
+++ src/mineshafter/proxy/MineProxyHandler.java
@@ -71,6 +71,8 @@ public class MineProxyHandler extends Thread {
 		Matcher audiofix_url = MineProxy.AUDIOFIX_URL.matcher(url);
 		Matcher dl_bukkit = MineProxy.DL_BUKKIT.matcher(url);
 		Matcher client_snoop = MineProxy.CLIENT_SNOOP.matcher(url);
+		Matcher server_snoop = MineProxy.SERVER_SNOOP.matcher(url);
+		
 		
 		byte[] data = null;
 		String contentType = null;
@@ -179,6 +181,27 @@ public class MineProxyHandler extends Thread {
 			System.out.println("ClientSnoop");
 			params = client_snoop.group(1);
 			url = "http://" + MineProxy.authServer + "/game/snoop/client"+params;
+			
+			System.out.println("To: " + url);
+			
+			try {
+				int postlen = Integer.parseInt(headers.get("content-length"));
+				char[] postdata = new char[postlen];
+				InputStreamReader reader = new InputStreamReader(fromClient);
+				reader.read(postdata);
+				
+				data = postRequest(url, new String(postdata), "application/x-www-form-urlencoded");
+				
+			} catch(IOException e) {
+				System.out.println("Unable to read POST data from getversion request");
+				
+				e.printStackTrace();
+			}
+		} else if(server_snoop.matches())
+		{
+			System.out.println("ServerSnoop");
+			params = server_snoop.group(1);
+			url = "http://" + MineProxy.authServer + "/game/snoop/server"+params;
 			
 			System.out.println("To: " + url);
 			
